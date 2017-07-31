bbc3dc09daea2936d1e320606841084e5ff6d774
davidwlee <sea0920@hotmail.com>
2017-07-31 01:23:23 -0700

    Basic Project Setup

---
+++ BloodSugarSimulator/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BloodSugarSimulator.Startup))]
+namespace BloodSugarSimulator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
